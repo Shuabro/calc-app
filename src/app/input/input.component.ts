da61acc7be2c8079bfee4232ae442ba52443437b
Joshua Powers <jpowerscts@gmail.com>
2023-02-07 10:41:39 -0500

    Before string methods

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { last } from 'rxjs';
 
 @Component({
   selector: 'app-input',
@@ -8,13 +9,77 @@ import { Component } from '@angular/core';
 export class InputComponent {
 
   result: string = '0';
+  input1: string = '';
+  input2: string = '';
+  placeholder: string = '';
+  answer : string = '';
+  lastKey: string = '';
+  operandKey: string = '';
 
- onClick(value: string){
-  if (value == "C"){
-    this.result = "0";
+  clickNum(value: string){
+    
+    if (this.input1 == '')
+    {
+      this.input1 = value;
+      this.result = this.input1;
+      this.placeholder = value;
+    }
 
-  };
+    else if (this.input2 == '')
+    {
+      this.result = this.result + value;
+      this.placeholder = this.placeholder + value;
+    }
 
- }
+    else
+    {
+      this.placeholder = this.placeholder + value;
+      this.result = this.result + value;
+    }
+
+    if (this.lastKey === "/" || this.lastKey ===  "x" || this.lastKey === "-" || this.lastKey === "+") 
+    {
+      this.input2 = value;
+      this.result = this.input2;
+      this.operandKey = this.lastKey;
+    }
+
+
+    
+
+  }
+
+  clear()
+  {
+    this.input1 = '';
+    this.input2 = '';
+    this.lastKey = '';
+    this.result = '0';
+    this.placeholder = '';
+
+  }
+
+  clickOperand(operand: string)
+  {
+    this.placeholder = this.result + operand;
+    this.lastKey = operand;
+
+
+  }
+
+  calculate()
+  {
+    if (this.answer == ''){
+      this.answer = eval(this.placeholder);
+      this.result = this.answer;
+    }
+
+    else {
+      console.log(this.input2);
+      this.placeholder = this.result + this.operandKey + this.input2;
+      this.result = eval(this.placeholder);
+    }
+  }
+ 
 
 }
